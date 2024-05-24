8c619845a7dbaff869182e5f2dfe85f96bdadc31
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2024-05-24 15:05:56 +0530

    Run the app in ci workflow (#2)

--- Program.ps1
+++ Program.ps1
@@ -1,7 +1,14 @@
+param(
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $Name
+)
+
 . "$PSScriptroot\Service.ps1"
 . "$PSScriptroot\Worker.ps1"
 
 $service = [Service]::new()
 $worker = [Worker]::new($service)
-$message = $worker.GetMessage("Jon1")
+$message = $worker.GetMessage($Name)
 Write-Output $message
