341e889ce3dcf32df8f62ac595dc287e4dfda514
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2024-05-24 14:48:41 +0530

    fix test

--- Service.Tests.ps1
+++ Service.Tests.ps1
@@ -7,6 +7,6 @@ BeforeAll {
 Describe "ServiceTests" {
 	It "Can_GetMessage_ReturnsProperMessage" {
 		$message = $service.GetMessage("Test User 1")
-		$message | Should -Be 'Hello from mock Test User 1'
+		$message | Should -Be 'Hello Test User 1'
 	}
 }
